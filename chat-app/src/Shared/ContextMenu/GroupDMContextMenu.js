9d42732fcb167e396c9bb9f7471ebc8280e47bbc
devmaxcat <chancespradley@hotmail.com>
2024-09-21 16:45:55 -0600

    Fatal Temp fix for no-undef error, will correct.

--- chat-app/src/Shared/ContextMenu/GroupDMContextMenu.js
+++ chat-app/src/Shared/ContextMenu/GroupDMContextMenu.js
@@ -13,9 +13,9 @@ export default function GroupDMContextMenu({ channel, context }) {
   const navigate = useNavigate()
   const requester = useContext(RequestContext)
 
-  let leave = () => {
-    modalservice.addModal(new Modal('CONFIRM', `Are you sure you want to unfriend ${user?.displayName || user.username}?\n\nThey will not be notified.`, [new ModalAction('Cancel', 'dismiss', 'secondary-grey'), new ModalAction('Confirm', (dismisser) => { friends.remove(user._id, true); dismisser() }, 'red')]));
-  }
+  // let leave = () => {
+  //   modalservice.addModal(new Modal('CONFIRM', `Are you sure you want to unfriend ${user?.displayName || user.username}?\n\nThey will not be notified.`, [new ModalAction('Cancel', 'dismiss', 'secondary-grey'), new ModalAction('Confirm', (dismisser) => { friends.remove(user._id, true); dismisser() }, 'red')]));
+  // }
  
 
   const buttons = [
